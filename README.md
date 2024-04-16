5d8eaf262031a49add2de42bf3c9e43661ae5b75
bbhitec <bboy.hitec@gmail.com>
2024-04-16 18:31:26 +0300

    feat: text to speech chain added

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Hugging Face multi-model utilization
 
-Multi model chaining with HuggingFace models pipelining
+Multi model chaining with HuggingFace models pipelining,
+specifically: "describe an image" using the chain:
+- Image to text generation (model: salesforce/blip-image-captioning-base)
+- Text to speech generation (model: espnet/kan-bayashi_ljspeech_vits)
+
 
 ![](https://shields.io/badge/-python-ffe600?logo=python)
 ![](https://shields.io/badge/-pytorch-4377cb?logo=pytorch)
 ![](https://shields.io/badge/-huggingface-4377cb?logo=huggingface)
 
+</br>
 
 ## Usage/How to run
 Pull and install required packages if needed *(virtual env recommended)*:
@@ -14,6 +19,6 @@ Pull and install required packages if needed *(virtual env recommended)*:
 pip install -r requirements.txt
 ```
 
-_*Additional dependency may be needed_
+_*Additional dependencies may be required_
 
 ###### [vnik]
--- app.py
+++ app.py
@@ -1,37 +1,59 @@
 '''
     [bbht] hugging-chain
     multi model chaining with HuggingFace models pipelining
+    specifically: "describe an image" using the chain:
+        -image to text generation (model: salesforce/blip-image-captioning-base)
+        -text to speech generation (model: espnet/kan-bayashi_ljspeech_vits)
 
 '''
 
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
 
+
+
+import requests
+import os
+
 load_dotenv(find_dotenv())
+HUGGING_FACE_ACCESS_TOKEN = os.getenv("HUGGING_FACE_ACCESS_TOKEN")
+
 
 ########## Image-to-text
 def img2text(url):
     img2text_pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
     
-    text_res = img2text_pipe(url)
+    text_res = img2text_pipe(url)[0]['generated_text']
 
-    print (text_res[0]['generated_text'])
+    print (text_res)
     return(text_res)
 
 
 
 
-########## Story generation (via LLM)
-
 
 
 ########## Story to Speach
+def text2speech(text):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGING_FACE_ACCESS_TOKEN}"}
+    payloads = {
+        "inputs": text
+    }
 
+    print (f"generating audio for: {text}")
+
+    response = requests.post(API_URL, headers=headers, json=payloads)
+    with open('audio_res.flac', 'wb') as file:
+        file.write(response.content)
 
 
 def main():
-    img_url = "https://drive.usercontent.google.com/download?id=1wWpoxNPxiXlqyDIJqT5iPs77IQMGcQj2"
-    img2text(img_url)
+    # img_url = "https://drive.usercontent.google.com/download?id=1wWpoxNPxiXlqyDIJqT5iPs77IQMGcQj2"
+    img_url = "pic1.jpg"
+    summary = img2text(img_url)
+    # story=text2story(summary)
+    text2speech(summary)
 
 if __name__ == "__main__":
     main()
