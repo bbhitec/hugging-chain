4cc88bb2c3408b93703cce8b3560a49a0904a63a
bbhitec <bboy.hitec@gmail.com>
2024-04-19 23:47:44 +0300

    feat: joke generation with langchain

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Hugging Face Multi-model Mtilization
 
-Multi model chaining with HuggingFace models pipelining,
-specifically: "describe an image" using the chain:
+Multi model chaining and with HuggingFace and LangChain
+Case specifically: "say a quick joke about the given image" using the tools:
 - Image to text generation (model: salesforce/blip-image-captioning-base)
+- Templated prompt text generation using LangChain with OpenAI model
 - Text to speech generation (model: espnet/kan-bayashi_ljspeech_vits)
 
+### Next
+- Demonstrate with a live app (via Flask?)
 
 </br>
 
@@ -13,19 +16,18 @@ specifically: "describe an image" using the chain:
 ![](https://shields.io/badge/-huggingface-4377cb?logo=huggingface)
 ![](https://shields.io/badge/-langchain-4377cb?logo=langchain)
 
-</br>
-
-## Next
-- Text to script generation
-- Web app introduction (via Flask?)
 
 ## Usage/How to run
-Pull and install required packages if needed *(virtual env recommended)*:
+Pull and install required packages if needed :
 
 ```python
 pip install -r requirements.txt
 ```
 
-_*Additional dependencies may be required_
+_*Additional dependencies may be required_</br>
+_*Additional environment variables may be needed_</br>
+_*Virtual env recommended_</br>
 
-###### [vnik]
+</br>
+
+##### [vnik]
--- app.py
+++ app.py
@@ -3,34 +3,50 @@
     multi model chaining with HuggingFace models pipelining
     specifically: "describe an image" using the chain:
         -image to text generation (model: salesforce/blip-image-captioning-base)
+        -templated prompt text generation using LangChain with OpenAI
         -text to speech generation (model: espnet/kan-bayashi_ljspeech_vits)
 
 '''
 
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 from transformers import pipeline
 
-
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 import requests
 import os
 
-load_dotenv(find_dotenv())
+load_dotenv()   # take environment variables from .env
 HUGGING_FACE_ACCESS_TOKEN = os.getenv("HUGGING_FACE_ACCESS_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 ########## Image-to-text
 def img2text(url):
-    img2text_pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-    
+    img2text_pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base", max_new_tokens=20)
+
     text_res = img2text_pipe(url)[0]['generated_text']
 
     print (text_res)
     return(text_res)
 
 
+########## Joke generation
+def text2story(summary):
 
+    model = ChatOpenAI(model="gpt-4")
 
+    prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+    output_parser = StrOutputParser()
+
+    chain = prompt | model | output_parser
+
+    story = chain.invoke({"topic": {summary}})
+
+    print(story)
+    return(story)
 
 
 ########## Story to Speach
@@ -46,14 +62,16 @@ def text2speech(text):
     response = requests.post(API_URL, headers=headers, json=payloads)
     with open('audio_res.flac', 'wb') as file:
         file.write(response.content)
+        print("file outputted!")
+
+
 
 
 def main():
-    # img_url = "https://drive.usercontent.google.com/download?id=1wWpoxNPxiXlqyDIJqT5iPs77IQMGcQj2"
     img_url = "pic1.jpg"
     summary = img2text(img_url)
-    # story=text2story(summary)
-    text2speech(summary)
+    story=text2story(summary)
+    text2speech(story)
 
 if __name__ == "__main__":
     main()
